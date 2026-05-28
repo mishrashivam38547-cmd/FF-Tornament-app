8c392ac2885b499c2a480f7224f8c48667de370a
mishrashivam38547-cmd <mishrashivam38547@gmail.com>
2026-05-28 17:37:32 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'rs7-esport-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
